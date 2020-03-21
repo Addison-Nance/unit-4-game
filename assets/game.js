f6172c69fbe36ddff82cc5bef2894692cc847fa4
Addison Nance <madi.nance911@gmail.com>
2020-03-21 14:05:22 -0600

    adding images, coding basic HTML structure, and starting javascript coding.

---
+++ assets/game.js
@@ -0,0 +1,29 @@
+var gem1;
+var gem2;
+var gem3;
+var gem4;
+var goal = 0;
+var score = 0;
+
+$( document ).ready(function() {
+    let goal = Math.floor((Math.random() * 50)+12)
+    console.log(goal)
+    $("#goalbox").text(goal)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
